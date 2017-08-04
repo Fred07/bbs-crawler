576b876a3c0829d253d1977432a8c829b1695e98
fred07 <fivesec10@gmail.com>
2017-08-05 02:15:28 +0800

    Fix coding style for python convention

--- app.py
+++ app.py
@@ -1,51 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 
-import pttClient
-import socket
+import pttclient
 import sys
 import os
 import yaml
 
 if __name__ == '__main__':
 
-	with open(os.path.dirname(__file__) + 'config/config.yaml', 'r') as f:
-		config = yaml.load(f)
+    with open(os.path.dirname(__file__) + 'config/config.yaml', 'r') as f:
+        config = yaml.load(f)
 
-		# Get parameters from config.yaml
-		host       = config['host_config']['host']
-		account    = config['host_config']['account']
-		password   = config['host_config']['password']
-		delayTime  = config['crawler_config']['cool_down_time']
-		removeCon  = config['crawler_config']['remove_prev_connect']
-		searchWord = config['crawler_config']['search_word']
-		board      = config['crawler_config']['board_list']
-		mode       = config['develop_config']['login_mode']
+        # Get parameters from config.yaml
+        host = config['host_config']['host']
+        account = config['host_config']['account']
+        password = config['host_config']['password']
+        delayTime = config['crawler_config']['cool_down_time']
+        removeCon = config['crawler_config']['remove_prev_connect']
+        searchWord = config['crawler_config']['search_word']
+        board = config['crawler_config']['board_list']
+        mode = config['develop_config']['login_mode']
 
+    pttHandler = pttclient.PttClient(host=host,
+                                     account=account,
+                                     pwd=password,
+                                     remove_prev_connect=removeCon,
+                                     mode=mode)
 
-	pttHandler = pttClient.pttClient(host = host,
-									 account = account,
-									 pwd = password,
-									 removePrevConnect = removeCon,
-									 mode = mode)
+    if not pttHandler.connect("ptt.cc"):
+        print(pttHandler.get_error_msg())
+        exit()
 
-	if (not pttHandler.connect("ptt.cc")):
-		print(pttHandler.getErrorMsg())
-		exit()
+    # Get search word from input
+    if len(sys.argv) > 1 and sys.argv[1]:
+        searchWord = sys.argv[1]
 
-	# Get search word from input
-	if (len(sys.argv) > 1 and sys.argv[1]):
-		searchWord = sys.argv[1]
+    pttHandler.login()
+    while pttHandler.is_login():
+        # pttHandler.control()
+        pttHandler.go_to_board(board)
+        keyWordCount = pttHandler.detect_wording(searchWord)
+        if keyWordCount >= 10:
+            print('警告!!!{:s}出現超過{:d}次!!'.format(searchWord, keyWordCount))
 
-
-	pttHandler.login()
-	while (pttHandler.isLogin()):
-			# pttHandler.control()
-			pttHandler.gotoBoard(board)
-			keyWordCount = pttHandler.detectWording(searchWord)
-			if (keyWordCount >= 10):
-				print('警告!!!{:s}出現超過{:d}次!!'.format(searchWord, keyWordCount))
-
-			print('冷卻開始...%d秒' % delayTime)
-			pttHandler.delay(delayTime)
-			print('冷卻結束')
+        print('冷卻開始...%d秒' % delayTime)
+        pttHandler.delay(delayTime)
+        print('冷卻結束')
